9b45f7fb291e2641890f9a3090025e65b158ec39
Thayanne Rego <136635862+thayanneregoo@users.noreply.github.com>
2026-03-23 15:59:38 -0300

    Add program to check if a number is even or odd

---
+++ questao15.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    int a;
+    printf("Informe numero inteiro : \n");
+    scanf("%d", &a);
+    if (a % 2 == 0) {
+        printf("Par \n");
+  
+    } 
+     else {
+        printf("Impar \n");
+    }
+
+
+    return 0;
+}
